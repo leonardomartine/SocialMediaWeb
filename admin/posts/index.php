fa3887e4b97cc06e0d099ef2a91f9ec413c7ebd6
leonardomartine <leonardomartine1508@gmail.com>
2022-10-11 12:11:57 +0700

    phase1
    updatee

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -1,15 +1,14 @@
-
-<?php if($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-</script>
-<?php endif;?>
+<?php if ($_settings->chk_flashdata('success')) : ?>
+	<script>
+		alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+	</script>
+<?php endif; ?>
 <div class="card card-outline rounded-0 card-navy">
 	<div class="card-header">
 		<h3 class="card-title">List of posts</h3>
 	</div>
 	<div class="card-body">
-        <div class="container-fluid">
+		<div class="container-fluid">
 			<table class="table table-hover table-striped table-bordered" id="list">
 				<colgroup>
 					<col width="5%">
@@ -32,28 +31,30 @@
 					</tr>
 				</thead>
 				<tbody>
-					<?php 
+					<?php
 					$i = 1;
-						$qry = $conn->query("SELECT p.*, concat(m.firstname, ' ', coalesce(concat(m.middlename,' '),''),m.lastname) as `name`, m.avatar, COALESCE((SELECT count(member_id) FROM `like_list` where post_id = p.id),0) as `likes`, COALESCE((SELECT count(member_id) FROM `comment_list` where post_id = p.id),0) as `comments` FROM post_list p inner join `member_list` m on p.member_id = m.id where p.member_id = '{$_settings->userdata('id')}' order by unix_timestamp(p.date_updated) desc");
-						while($row = $qry->fetch_assoc()):
+					$qry = $conn->query("SELECT p.*, concat(m.firstname, ' ', coalesce(concat(m.middlename,' '),''),m.lastname) as `name`, m.avatar, COALESCE((SELECT count(member_id) FROM `like_list` where post_id = p.id),0) as `likes`, COALESCE((SELECT count(member_id) FROM `comment_list` where post_id = p.id),0) as `comments` FROM post_list p inner join `member_list` m on p.member_id = m.id where p.member_id = '{$_settings->userdata('id')}' order by unix_timestamp(p.date_updated) desc");
+					while ($row = $qry->fetch_assoc()) :
 					?>
 						<tr>
 							<td class="text-center"><?php echo $i++; ?></td>
-							<td><?php echo date("Y-m-d H:i",strtotime($row['date_created'])) ?></td>
+							<td><?php echo date("Y-m-d H:i", strtotime($row['date_created'])) ?></td>
 							<td><?php echo $row['name'] ?></td>
-							<td class=""><p class="m-0 truncate-1"><?= $row['caption'] ?></p></td>
+							<td class="">
+								<p class="m-0 truncate-1"><?= $row['caption'] ?></p>
+							</td>
 							<td class="text-right"><?php echo format_num($row['likes']) ?></td>
 							<td class="text-right"><?php echo format_num($row['comments']) ?></td>
 							<td align="center">
-								 <button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-				                  		Action
-				                    <span class="sr-only">Toggle Dropdown</span>
-				                  </button>
-				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item view_data" href="./?page=posts/view_post&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
-				                  </div>
+								<button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+									Action
+									<span class="sr-only">Toggle Dropdown</span>
+								</button>
+								<div class="dropdown-menu" role="menu">
+									<a class="dropdown-item view_data" href="./?page=posts/view_post&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+								</div>
 							</td>
 						</tr>
 					<?php endwhile; ?>
@@ -63,35 +64,39 @@
 	</div>
 </div>
 <script>
-	$(document).ready(function(){
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this post permanently?","delete_post",[$(this).attr('data-id')])
+	$(document).ready(function() {
+		$('.delete_data').click(function() {
+			_conf("Are you sure to delete this post permanently?", "delete_post", [$(this).attr('data-id')])
 		})
 		$('.table').dataTable({
-			columnDefs: [
-					{ orderable: false, targets: [4] }
-			],
-			order:[0,'asc']
+			columnDefs: [{
+				orderable: false,
+				targets: [4]
+			}],
+			order: [0, 'asc']
 		});
 		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
 	})
-	function delete_post($id){
+
+	function delete_post($id) {
 		start_loader();
 		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_post",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error:err=>{
+			url: _base_url_ + "classes/Master.php?f=delete_post",
+			method: "POST",
+			data: {
+				id: $id
+			},
+			dataType: "json",
+			error: err => {
 				console.log(err)
-				alert_toast("An error occured.",'error');
+				alert_toast("An error occured.", 'error');
 				end_loader();
 			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
+			success: function(resp) {
+				if (typeof resp == 'object' && resp.status == 'success') {
 					location.reload();
-				}else{
-					alert_toast("An error occured.",'error');
+				} else {
+					alert_toast("An error occured.", 'error');
 					end_loader();
 				}
 			}
--- admin/user/list.php
+++ admin/user/list.php
@@ -1,25 +1,25 @@
-<?php if($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-</script>
-<?php endif;?>
+<?php if ($_settings->chk_flashdata('success')) : ?>
+	<script>
+		alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+	</script>
+<?php endif; ?>
 <style>
-    .user-avatar{
-        width:3rem;
-        height:3rem;
-        object-fit:scale-down;
-        object-position:center center;
-    }
+	.user-avatar {
+		width: 3rem;
+		height: 3rem;
+		object-fit: scale-down;
+		object-position: center center;
+	}
 </style>
 <div class="card card-outline rounded-0 card-navy">
 	<div class="card-header">
 		<h3 class="card-title">List of Users</h3>
 		<div class="card-tools">
-			<a href="./?page=user/manage_user" id="create_new" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
+			<a href="./?page=user/manage_user" id="create_new" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span> Create New</a>
 		</div>
 	</div>
 	<div class="card-body">
-        <div class="container-fluid">
+		<div class="container-fluid">
 			<table class="table table-hover table-striped table-bordered" id="list">
 				<colgroup>
 					<col width="5%">
@@ -42,38 +42,38 @@
 					</tr>
 				</thead>
 				<tbody>
-					<?php 
+					<?php
 					$i = 1;
-						$qry = $conn->query("SELECT *, concat(firstname,' ', lastname) as `name` from `users` where id != '{$_settings->userdata('id')}' order by concat(firstname,' ', lastname) asc ");
-						while($row = $qry->fetch_assoc()):
+					$qry = $conn->query("SELECT *, concat(firstname,' ', lastname) as `name` from `users` where id != '{$_settings->userdata('id')}' order by concat(firstname,' ', lastname) asc ");
+					while ($row = $qry->fetch_assoc()) :
 					?>
 						<tr>
 							<td class="text-center"><?php echo $i++; ?></td>
-							<td><?php echo date("Y-m-d H:i",strtotime($row['date_updated'])) ?></td>
+							<td><?php echo date("Y-m-d H:i", strtotime($row['date_updated'])) ?></td>
 							<td class="text-center">
-                                <img src="<?= validate_image($row['avatar']) ?>" alt="" class="img-thumbnail rounded-circle user-avatar">
-                            </td>
+								<img src="<?= validate_image($row['avatar']) ?>" alt="" class="img-thumbnail rounded-circle user-avatar">
+							</td>
 							<td><?php echo $row['name'] ?></td>
 							<td><?php echo $row['username'] ?></td>
 							<td class="text-center">
-                                <?php if($row['type'] == 1): ?>
-                                    Administrator
-                                <?php elseif($row['type'] == 2): ?>
-                                    Staff
-                                <?php else: ?>
+								<?php if ($row['type'] == 1) : ?>
+									Administrator
+								<?php elseif ($row['type'] == 2) : ?>
+									Staff
+								<?php else : ?>
 									N/A
-                                <?php endif; ?>
-                            </td>
+								<?php endif; ?>
+							</td>
 							<td align="center">
-								 <button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-				                  		Action
-				                    <span class="sr-only">Toggle Dropdown</span>
-				                  </button>
-				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item" href="./?page=user/manage_user&id=<?= $row['id'] ?>"><span class="fa fa-edit text-dark"></span> Edit</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
-				                  </div>
+								<button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+									Action
+									<span class="sr-only">Toggle Dropdown</span>
+								</button>
+								<div class="dropdown-menu" role="menu">
+									<a class="dropdown-item" href="./?page=user/manage_user&id=<?= $row['id'] ?>"><span class="fa fa-edit text-dark"></span> Edit</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+								</div>
 							</td>
 						</tr>
 					<?php endwhile; ?>
@@ -83,34 +83,38 @@
 	</div>
 </div>
 <script>
-	$(document).ready(function(){
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this User permanently?","delete_user",[$(this).attr('data-id')])
+	$(document).ready(function() {
+		$('.delete_data').click(function() {
+			_conf("Are you sure to delete this User permanently?", "delete_user", [$(this).attr('data-id')])
 		})
 		$('.table').dataTable({
-			columnDefs: [
-					{ orderable: false, targets: [6] }
-			],
-			order:[0,'asc']
+			columnDefs: [{
+				orderable: false,
+				targets: [6]
+			}],
+			order: [0, 'asc']
 		});
 		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
 	})
-	function delete_user($id){
+
+	function delete_user($id) {
 		start_loader();
 		$.ajax({
-			url:_base_url_+"classes/Users.php?f=delete",
-			method:"POST",
-			data:{id: $id},
-			error:err=>{
+			url: _base_url_ + "classes/Users.php?f=delete",
+			method: "POST",
+			data: {
+				id: $id
+			},
+			error: err => {
 				console.log(err)
-				alert_toast("An error occured.",'error');
+				alert_toast("An error occured.", 'error');
 				end_loader();
 			},
-			success:function(resp){
-				if(resp == 1){
+			success: function(resp) {
+				if (resp == 1) {
 					location.reload();
-				}else{
-					alert_toast("An error occured.",'error');
+				} else {
+					alert_toast("An error occured.", 'error');
 					end_loader();
 				}
 			}
